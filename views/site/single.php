b7c620697a21f34da677280bec42d2fcecbc24b7
Минчик Эдуард <min4ik.edward@gmail.com>
2019-07-01 01:41:59 +0300

    # подключена бд, операции crud для записей, загрузка изображений в записи # страница админа # главная страница с записями, существует пагинация

--- views/site/single.php
+++ views/site/single.php
@@ -18,7 +18,7 @@
 
                         </header>
                         <div class="entry-content">
-                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                            <p>Lorem ipsum dolor sit amet,  sadipscing elitr, sed diam nonumy eirmod
                                 tevidulabore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
                                 justo duo dolores rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
                                 ipsum dolor sit am Lorem ipsum dolor sitconsetetur sadipscing elitr, sed diam nonumy
